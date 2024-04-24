e57d4aba28adc76756a02b125aef30fbfd565126
prvladko <prvladko@gmail.com>
2024-04-24 08:54:17 +0300

    minor refactor

--- lib/fuel_calculator.ex
+++ lib/fuel_calculator.ex
@@ -3,13 +3,19 @@ defmodule FuelCalculator do
   Calculates the fuel required for a space mission based on a series of launch and landing steps.
   """
 
-  @gravities %{
-    "earth" => 9.807,
-    "moon" => 1.62,
-    "mars" => 3.711
-  }
+  @gravities %{"earth" => 9.807, "moon" => 1.62, "mars" => 3.711}
 
-  # Calculates initial fuel for a single action and floors the result immediately.
+  @doc """
+  Calculates the fuel required for a single launch or landing action and floors the result immediately.
+
+  ## Examples
+      iex> FuelCalculator.calculate_fuel(28801, 9.807, :launch)
+      11829
+
+      iex> FuelCalculator.calculate_fuel(28801, 1.62, :land)
+      1497
+  """
+  @spec calculate_fuel(mass :: number, gravity :: number, action :: :launch | :land) :: number
   def calculate_fuel(mass, gravity, action) do
     fuel =
       case action do
@@ -20,7 +26,20 @@ defmodule FuelCalculator do
     floor(max(fuel, 0))
   end
 
-  # Entry point for fuel requirement calculation.
+  @doc """
+  Entry point for fuel requirement calculation.
+
+  ## Examples
+      iex> FuelCalculator.calculate_fuel_requirement(28801, [{:launch, "earth"}, {:land, "moon"}, {:launch, "moon"}, {:land, "earth"}])
+      {:ok, 51898}
+
+      iex> FuelCalculator.calculate_fuel_requirement(14606, [{:launch, "earth"}, {:land, "mars"}, {:launch, "mars"}, {:land, "earth"}])
+      {:ok, 33388}
+  """
+  @spec calculate_fuel_requirement(
+          mass :: number,
+          steps :: list({:launch | :land, String.t()})
+        ) :: {:ok, number} | {:error, String.t()}
   def calculate_fuel_requirement(mass, steps) do
     case validate_steps(steps, @gravities) do
       {:ok} ->
@@ -40,11 +59,17 @@ defmodule FuelCalculator do
     end
   end
 
-  # Recursively calculates total fuel, adding the total to each step.
+  @spec total_fuel(mass :: number, gravity :: number, action :: :launch | :land) :: number
   defp total_fuel(mass, gravity, action) do
     additional_fuel(mass, gravity, action, 0)
   end
 
+  @spec additional_fuel(
+          mass :: number,
+          gravity :: number,
+          action :: :launch | :land,
+          added_fuel :: number
+        ) :: number
   defp additional_fuel(mass, gravity, action, added_fuel) do
     new_fuel = calculate_fuel(mass, gravity, action)
 
@@ -55,6 +80,10 @@ defmodule FuelCalculator do
     end
   end
 
+  @spec validate_steps(
+          steps :: list({:launch | :land, String.t()}),
+          gravities :: map()
+        ) :: {:ok} | {:error, String.t()}
   defp validate_steps(steps, gravities) do
     try do
       Enum.each(steps, fn {_action, planet} ->
@@ -63,7 +92,8 @@ defmodule FuelCalculator do
 
       {:ok}
     rescue
-      RuntimeError -> {:error, "Invalid route format"}
+      RuntimeError ->
+        {:error, "Invalid route format"}
     end
   end
 end
--- test/fuel_calculator_test.exs
+++ test/fuel_calculator_test.exs
@@ -1,18 +1,29 @@
 defmodule FuelCalculatorTest do
   use ExUnit.Case, async: true
+  doctest FuelCalculator
 
   describe "calculate_fuel/2" do
-    test "calculate total mission fuel for Apollo 11" do
+    test "calculates fuel correctly for launch on Earth" do
+      assert FuelCalculator.calculate_fuel(28801, 9.807, :launch) == 11829
+    end
+
+    test "calculates fuel correctly for landing on Moon" do
+      assert FuelCalculator.calculate_fuel(28801, 1.62, :land) == 1497
+    end
+  end
+
+  describe "calculate_fuel_requirement/2" do
+    test "calculates total fuel for Apollo 11 mission" do
       route1 = [{:launch, "earth"}, {:land, "moon"}, {:launch, "moon"}, {:land, "earth"}]
       assert FuelCalculator.calculate_fuel_requirement(28801, route1) == {:ok, 51898}
     end
 
-    test "Mars route" do
+    test "calculates total fuel for Mars mission" do
       route2 = [{:launch, "earth"}, {:land, "mars"}, {:launch, "mars"}, {:land, "earth"}]
       assert FuelCalculator.calculate_fuel_requirement(14606, route2) == {:ok, 33388}
     end
 
-    test "Passenger ship" do
+    test "calculates total fuel for passenger ship mission" do
       route3 = [
         {:launch, "earth"},
         {:land, "moon"},
@@ -37,9 +48,26 @@ defmodule FuelCalculatorTest do
                {:error, "Invalid route format"}
     end
 
-    test "handling unknown planet gracefully" do
+    test "returns error when route starts with unknown planet" do
       assert FuelCalculator.calculate_fuel_requirement(28801, [{:launch, "pluto"}]) ==
                {:error, "Invalid route format"}
+    end
+
+    test "returns error when route has multiple unknown planets" do
+      invalid_route = [
+        {:launch, "earth"},
+        {:land, "jupiter"},
+        {:launch, "saturn"},
+        {:land, "earth"}
+      ]
+
+      assert FuelCalculator.calculate_fuel_requirement(14606, invalid_route) ==
+               {:error, "Invalid route format"}
+    end
+
+    test "returns {:ok, 0} when no fuel is required" do
+      route = [{:launch, "earth"}, {:land, "moon"}]
+      assert FuelCalculator.calculate_fuel_requirement(0, route) == {:ok, 0}
     end
   end
 end
